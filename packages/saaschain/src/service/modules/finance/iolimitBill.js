c7cc38f9cca5e04b106cd0788392aa9155f13752
hd <289639798@qq.com>
2021-12-21 16:50:26 +0800

    门店支出上限设置

---
+++ packages/saaschain/src/service/modules/finance/iolimitBill.js
@@ -0,0 +1,40 @@
+import yid from '@src/library'
+
+const prefix = 'api-finance/reservebillhead';
+
+export function billList(params) {
+    return yid.http.get(`${prefix}/billList`, {params})
+}
+
+export function listAll(params) {
+    return yid.http.get(`${prefix}/list/all`, {params})
+}
+
+export function saveBill(params) {
+    const url = `${prefix}/saveBill`
+    return yid.http.post(url, params )
+}
+
+export function saveSystem(params) {
+    const url = `${prefix}/saveSystem`
+    return yid.http.post(url, params )
+}
+
+export function deleteBill(params) {
+    const url = `${prefix}/deleteBill`
+    return yid.http.post(url,params)
+}
+
+export function auditBill(params) {
+    const url = `${prefix}/auditBill`
+    return yid.http.post(url, params )
+}
+
+export default {
+    billList,
+    listAll,
+    saveBill,
+    saveSystem,
+    deleteBill,
+    auditBill,
+}

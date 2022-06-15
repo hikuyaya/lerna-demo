e45209cd66d96859c376b2020d362588ad4cb35c
hikuyaya <hikuyaya@163.com>
2022-06-15 16:03:47 +0800

    chore: 引入可选链、prettier

--- packages/personnelweb/babel.config.js
+++ packages/personnelweb/babel.config.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: wqy
+ * @Date: 2022-06-15 14:05:17
+ * @LastEditors: wqy
+ * @LastEditTime: 2022-06-15 15:25:37
+ * @FilePath: \personnelweb\babel.config.js
+ * @Description:
+ */
 module.exports = {
-  presets: ['@vue/app']
+  presets: ['@vue/app'],
+  plugins: ['@babel/plugin-proposal-optional-chaining']
 }

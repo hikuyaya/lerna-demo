dadf5cb4f070ebf3c8169e2b040281096a8be954
hikuyaya <hikuyaya@163.com>
2022-08-31 17:07:22 +0800

    update

---
+++ packages/admin/src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wqy
+ * @Date: 2022-08-30 13:53:04
+ * @LastEditors: wqy
+ * @LastEditTime: 2022-08-30 13:53:25
+ * @FilePath: \lerna-demo\packages\admin\src\index.js
+ * @Description:
+ */
+import { Comm } from "@demo/common";
+
+Comm();
---
+++ packages/common/src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wqy
+ * @Date: 2022-08-30 13:52:22
+ * @LastEditors: wqy
+ * @LastEditTime: 2022-08-30 13:52:41
+ * @FilePath: \lerna-demo\packages\common\src\index.js
+ * @Description:
+ */
+export const Comm = () => {
+  console.log("comm-------------");
+};
